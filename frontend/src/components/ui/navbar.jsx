e9f2bfe0b681181fe8cd5a7f3e3ced59e593d50e
jamshiee <jamsheedjaleel03@gmail.com>
2025-05-01 15:21:57 +0530

    Final Commit

--- frontend/src/components/ui/navbar.jsx
+++ frontend/src/components/ui/navbar.jsx
@@ -231,8 +231,8 @@ export default function Example() {
               aria-current={item.current ? "page" : undefined}
               className={classNames(
                 item.current
-                  ? "bg-gray-900 text-white"
-                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  ? "bg-gray-200 text-gray-700"
+                  : "bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-white",
                 "block rounded-md px-3 py-2 text-base font-medium"
               )}
             >
