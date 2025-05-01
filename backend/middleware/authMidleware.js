866cbd5a8e7275b9d7a58115fcaef191052cc84b
jamshiee <jamsheedjaleel03@gmail.com>
2025-05-01 17:43:56 +0530

    Bugs Fixed

--- backend/middleware/authMidleware.js
+++ backend/middleware/authMidleware.js
@@ -4,18 +4,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const authMiddleware = async (req, res, next) => {
-  const authHeader = req?.headers?.cookie;
+  // const authHeader = req?.headers?.cookie;
 
 
-  if (!authHeader || !authHeader?.startsWith("jwt")) {
+  // if (!authHeader || !authHeader?.startsWith("jwt")) {
+  //   return res.status(401).json({
+  //     message: "Authentication failed",
+  //   });
+  // }
+
+  // const token = authHeader?.split("=")[1];
+  
+  const token = req.cookies.jwt;
+
+  if (!token) {
     return res.status(401).json({
       message: "Authentication failed",
     });
   }
-
-  const token = authHeader?.split("=")[1];
   
-
 
 //   const authHeader = req?.headers?.authorization;
 
@@ -27,18 +34,27 @@ const authMiddleware = async (req, res, next) => {
 
 //   const token = authHeader?.split(" ")[1];
 
+  // try {
+  //   const userToken = jwt.verify(token, process.env.JWT_SECRET);
+  
+  //   req.user = {
+  //     userId: userToken.userId,
+  //   };
+  //   next();
+  // } catch (error) {
+  //   console.log(error);
+  //   return res.status(401).json({
+  //     message: "Authentication Internal Failed",
+  //   });
+  // }
+
   try {
     const userToken = jwt.verify(token, process.env.JWT_SECRET);
-  
-    req.user = {
-      userId: userToken.userId,
-    };
+    req.user = { userId: userToken.userId };
     next();
   } catch (error) {
-    console.log(error);
-    return res.status(401).json({
-      message: "Authentication Internal Failed",
-    });
+    return res.status(401).json({ message: "Authentication Internal Failed" });
   }
+  
 };
 export default authMiddleware;
